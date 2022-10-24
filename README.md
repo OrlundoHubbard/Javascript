fe24d4f0671eff61c4aaa9d06a9903c915ea8986
Orlundo Hubbard <orlundokhubbard@gmail.com>
2022-10-23 19:28:23 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## Javascript
+
+Just some javascript practice 
---
+++ main.js
@@ -0,0 +1,7 @@
+const cookies = 2;
+
+if (cookies <= 3) {
+  console.log("I'll have just 1, please.");
+} else {
+  console.log("3 is too many");
+}
